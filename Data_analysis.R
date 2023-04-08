d370f976aa8e7589a2ee77b3da6aedc1bd1cdc38
nikakoga <weronikakoga@gmail.com>
2023-04-08 23:12:59 +0200

    liczenie grup

--- Data_analysis.R
+++ Data_analysis.R
@@ -1,3 +1,16 @@
 setwd("C:/Users/weron/Documents/Studia/PP/4 semestr/SAD/Projekt")
-dane<-read.csv2("Dane.csv")
+dane<-read.csv2("Dane.csv",header=TRUE)
 dane
+
+
+#This way I can count how many groups there is
+names(dane)[1] <- "ID"
+my_table<-table(dane$ID)
+my_table
+how_many_groups<-length(names(my_table))
+how_many_groups
+
+for (col_name in names(dane)) {
+  cat("Column ", col_name, ":\n")
+  print(dane[[col_name]])
+}
